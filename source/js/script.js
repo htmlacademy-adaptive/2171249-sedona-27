7fc49c1664f6be16e6572be1740beb3e9158d22e
Viktor Babenko <baumschule103@gmail.com>
2023-01-20 16:27:55 +0300

    Final

--- source/js/script.js
+++ source/js/script.js
@@ -1,9 +1,10 @@
 const menu = document.querySelector('.navigation');
 const menuBtn = document.querySelector('.navigation__btn');
+const body = document.querySelector('body');
 
-// if (document.querySelector('.no-js')) {
-//   body.classList.remove('no-js');
-// }
+if (document.querySelector('.no-js')) {
+  body.classList.remove('no-js');
+}
 
 if (menuBtn) {
   menuBtn.addEventListener('click', function (evt) {
